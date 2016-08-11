2fcd472ddafd4dcc5dc39119ce1d847928b3af9d
weikun <wk2003119@163.com>
2016-08-12 07:13:29 +0800

    812

--- C18/src/main/java/com/weikun/dao/IUserDAO.java
+++ C18/src/main/java/com/weikun/dao/IUserDAO.java
@@ -1,8 +1,8 @@
 package com.weikun.dao;
 
 public interface IUserDAO {
-	public void add();
+	
 	public void del() ;
 	public void update() ;
-	public void queryAll() ;
+	public void queryAll() ;public void add();
 }
